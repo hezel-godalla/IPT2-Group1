45646c305210df7d54d3839699fa0a4e5fd14858
hezel-godalla <hezelgodalla@gmail.com>
2025-03-19 16:55:33 +0800

    update changes registry.php

---
+++ database/registry.php
@@ -0,0 +1,14 @@
+<?php
+  $server_name = "localhost";
+  $db_name = "ipt2_g1";
+  $db_username = "root";
+  $db_password = "";
+
+  $conn = new mysqli($server_name, $db_username, $db_password, $db_name);
+  
+  if($conn->connect_error){
+    die("Database connection failed. " . $conn->connect_error);
+  }
+
+
+?>
